9ce88ce67d2fe1354527726bf1fb7b7251248284
carpicoder <webcarpi@gmail.com>
2023-01-26 05:55:11 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
